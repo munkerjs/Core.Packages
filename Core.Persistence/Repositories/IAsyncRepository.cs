ce16b5708c3bc0e0e16807ec534127eba7b932cc
Münker Erkadam <munkererkadam@gmail.com>
2024-03-15 11:55:12 +0300

    Dynamic Query altyapısı hazırlandı.

--- Core.Persistence/Repositories/IAsyncRepository.cs
+++ Core.Persistence/Repositories/IAsyncRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
