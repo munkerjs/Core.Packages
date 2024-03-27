8fbdebe072bb242b5f9f45b8fb2776450017ed37
Münker Erkadam <munkererkadam@gmail.com>
2024-03-27 12:18:01 +0300

    Add Caching Operasyonu

---
+++ Core.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching;
+
+public interface ICachableRequest
+{
+    string CacheKey { get; set; }
+    bool BypassCache { get; set; }
+    TimeSpan? SlidingExpiration { get; }
+}
